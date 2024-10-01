7479b3a8469512118119bbe053e7ca0b6c1fb6c1
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2024-10-01 09:50:03 +0300

    + FastSort and WordsSuffix variants of WordStat homework

--- java/wordStat/WordStatTest.java
+++ java/wordStat/WordStatTest.java
@@ -20,9 +20,30 @@ public final class WordStatTest {
     private static final Named<Comparator<Pair<String, Integer>>> INPUT = Named.of("Input", Comparator.comparingInt(p -> 0));
 
 
+    /// Words
+    private static final Named<Comparator<Pair<String, Integer>>> WORDS =
+            Named.of("Words", Comparator.comparing(Pair<String, Integer>::first).reversed());
+
+    /// Suffix
+    public static final int SIZE = 3;
+
+    static Named<Function<String, Stream<String>>> length(
+            final String name,
+            final int length,
+            final Function<String, Stream<String>> lng,
+            final Function<String, Stream<String>> shrt
+    ) {
+        return Named.of(name, s -> (s.length() >= length ? lng : shrt).apply(s));
+    }
+
+    private static final Named<Function<String, Stream<String>>> SUFFIX =
+            length("Suffix", SIZE, s -> Stream.of(s.substring(s.length() - SIZE)), Stream::of);
+
+
     /// Common
     public static final Selector SELECTOR = new Selector(WordStatTester.class)
             .variant("Base",            WordStatTester.variant(INPUT, ID))
+            .variant("WordsSuffix",     WordStatTester.variant(WORDS, SUFFIX))
             ;
 
     private WordStatTest() {
