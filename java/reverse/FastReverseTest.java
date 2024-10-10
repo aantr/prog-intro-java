2b494c040426db6cb20e6d0ddb395105016b02fa
Nikolay Vedernikov <VedernikovNV@gmail.com>
2024-10-10 13:30:21 +0300

    + MaxOct variant for FastReverse homework

--- java/reverse/FastReverseTest.java
+++ java/reverse/FastReverseTest.java
@@ -21,6 +21,7 @@ import static reverse.ReverseTest.M;
 public final class FastReverseTest {
     /// Max
 
+    private static final Named<Op> MAX = max("", a -> a, Integer.MIN_VALUE);
     private static final Named<Op> MAX_ABS = max("Abs", Math::abs, 0);
     private static final Named<Op> MAX_MOD = max("Mod", a -> (a % M + M) % M, 0);
     private static final Named<Op> MAX_ABS_MOD = max("AbsMod", a -> Math.abs(a) % M, 0);
@@ -46,6 +47,9 @@ public final class FastReverseTest {
         });
     }
 
+    /// Octal
+
+    private static final Named<BiFunction<ExtendedRandom, Integer, String>> OCT_IN = Named.of("Oct", (r, i) -> Integer.toOctalString(i));
     private static final Named<BiFunction<ExtendedRandom, Integer, String>> OCT_OUT = Named.of("Oct", (r, i) -> Integer.toOctalString(i) + "o");
     private static final Named<BiFunction<ExtendedRandom, Integer, String>> OCT_DEC = Named.of("OctDec", (r, i) ->
             r.nextBoolean() ? Integer.toString(i) : toOctalString(r, i));
@@ -61,6 +65,7 @@ public final class FastReverseTest {
             .variant("MaxAbsOctDec",    ReverseTester.variant(MAX_SIZE, "", MAX_ABS, OCT_DEC, OCT_OUT))
             .variant("MaxModOctDec",    ReverseTester.variant(MAX_SIZE, "", MAX_MOD, OCT_DEC, OCT_OUT))
             .variant("MaxAbsModOctDec", ReverseTester.variant(MAX_SIZE, "", MAX_ABS_MOD, OCT_DEC, OCT_OUT))
+            .variant("MaxOct",          ReverseTester.variant(MAX_SIZE, "", MAX, OCT_IN, OCT_IN))
             ;
 
     private FastReverseTest() {
