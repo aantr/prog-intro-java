7af18ed23aa1cd31385a41a36854b72af6c2882b
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2024-10-29 18:06:20 +0300

    Checked by an instructor

--- java-solutions/markup/ListItem.java
+++ java-solutions/markup/ListItem.java
@@ -3,12 +3,12 @@ package markup;
 import java.util.List;
 
 public class ListItem extends DocBookBuilder implements DocBook {
-    public ListItem(List<ListItemBase> elements) {
+    public ListItem(final List<ListItemBase> elements) {
         super(elements);
     }
 
     @Override
-    public void toDocBook(StringBuilder stringBuilder) {
+    public void toDocBook(final StringBuilder stringBuilder) {
         buildDocBook(stringBuilder, "listitem");
     }
 }
--- java-solutions/markup/MarkupBase.java
+++ java-solutions/markup/MarkupBase.java
@@ -5,27 +5,27 @@ import java.util.List;
 public class MarkupBase {
     private final List<? extends Element> elements;
 
-    public MarkupBase(List<? extends Element> elements) {
+    public MarkupBase(final List<? extends Element> elements) {
         this.elements = elements;
     }
 
-    public void buildMarkdown(StringBuilder stringBuilder, String symbol) {
+    public void buildMarkdown(final StringBuilder stringBuilder, final String symbol) {
         stringBuilder.append(symbol);
-        for (Element el : elements) {
+        for (final Element el : elements) {
             el.toMarkdown(stringBuilder);
         }
         stringBuilder.append(symbol);
     }
 
-    public void buildDocBook(StringBuilder stringBuilder, String tag, String role) {
+    public void buildDocBook(final StringBuilder stringBuilder, final String tag, final String role) {
         stringBuilder.append('<').append(tag).append(role != null ? String.format(" role='%s'", role) : "").append('>');
-        for (Element el : elements) {
+        for (final Element el : elements) {
             el.toDocBook(stringBuilder);
         }
         stringBuilder.append("</").append(tag).append('>');
     }
 
-    public void buildDocBook(StringBuilder stringBuilder, String tag) {
+    public void buildDocBook(final StringBuilder stringBuilder, final String tag) {
         buildDocBook(stringBuilder, tag, null);
     }
 }
