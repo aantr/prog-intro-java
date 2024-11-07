10ec32f07eb2fcc77b097334aade4b0acacd8b23
aantr <xgamestudio8@gmail.com>
2024-11-07 19:43:06 +0300

    added

---
+++ java-solutions/contest/B.java
@@ -0,0 +1,16 @@
+package contest;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        final int howmuch = 710;
+        int now = -710 * 25000;
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            System.out.println(now + i * (howmuch));
+        }
+    }
+}
