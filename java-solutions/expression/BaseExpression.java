375655587eafab30edab5e5bb733664e6e431b09
aantr <xgamestudio8@gmail.com>
2024-11-26 17:44:06 +0300

    expression clear git

--- java-solutions/expression/BaseExpression.java
+++ java-solutions/expression/BaseExpression.java
@@ -1,8 +1,15 @@
 package expression;
 
 public abstract class BaseExpression implements Expression, TripleExpression, FloatMapExpression {
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof BaseExpression)) {
+            return false;
+        }
+        return toString().equals(obj.toString());
+    }
     public boolean equals(BaseExpression baseExpression) {
-        return toString().equals(baseExpression.toString());
+        return equals((Object) baseExpression);
     }
 
     @Override
